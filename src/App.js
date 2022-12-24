652661594feb6aaab8712f42cc158d9bd5d9e07b
andi <78794419+andiks2018@users.noreply.github.com>
2022-12-24 10:17:20 +0700

    hapus route parsinta

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom'
 import './App.css';
-import Parsinta from './pages/Parsinta';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<h1>Hello</h1>} />
-      <Route path='/app' element={<Parsinta />} />
     </Routes>
   );
 }
